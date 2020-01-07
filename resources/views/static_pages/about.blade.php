ee98d17cc9e258c8c6bddb6df97d90593f0290ca
vagrant <vagrant@homestead>
2020-01-07 07:38:41 +0000

    as

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'about')
+
+@section('content')
+  <h1>about</h1>
+@endsection
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'help')
+
+@section('content')
+  <h1>帮助夜</h1>
+@endsection
